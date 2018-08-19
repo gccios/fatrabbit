6a0f25a8ee19bf672b3ebf3618ffbaa1e40b3c2e
郭春城 <gcc@lifesea.com>
2018-08-19 19:28:49 +0800

    Initial Commit

---
+++ fatrabbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fatrabbit
+//
+//  Created by 郭春城 on 2018/8/19.
+//  Copyright © 2018年 郭春城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
